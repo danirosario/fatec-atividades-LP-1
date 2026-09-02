b7ed3e0bdd0b24abb0e156233b559cebcead670d
danirosario <rosario.dani01@gmail.com>
2026-09-02 11:30:37 -0300

    Refactor OiTchau.java to include Oi and Tchau classes with methods for displaying messages

--- exercicios01-introducao/OiTchau/src/OiTchau.java
+++ exercicios01-introducao/OiTchau/src/OiTchau.java
@@ -1,7 +1,41 @@
 public class OiTchau 
 {
-    public static void main(String[] args) throws Exception 
+    public static void main(String[] args) 
     {
-        
+        Oi oi = new Oi("Oi");
+        oi.exibirOi();
+
+        Tchau tchau = new Tchau("Tchau");
+        tchau.exibirTchau();
+    }
+}
+
+class Oi 
+{
+    private String mensagemOi;
+
+    public Oi(String mensagemOi) 
+    {
+        this.mensagemOi = mensagemOi;
+    }
+
+    public void exibirOi() 
+    {
+        System.out.println(mensagemOi);
+    }
+}
+
+class Tchau 
+{
+    private String mensagemTchau;
+
+    public Tchau(String mensagemTchau) 
+    {
+        this.mensagemTchau = mensagemTchau;
+    }
+
+    public void exibirTchau() 
+    {
+        System.out.println(mensagemTchau);
     }
 }
