f4fce023ec89b5cea576ed656c2bd81fea35c418
danirosario <rosario.dani01@gmail.com>
2026-09-03 22:52:43 -0300

    add the exercises DuasMensagens.java, FrasePreferida.java, and Fatec.java; Included Lista01-Introducao-respostas-multiplaescolha.docx for multiple choice answers.

---
+++ exercicios01-introducao/FrasePreferida/src/FrasePreferida.java
@@ -0,0 +1,7 @@
+public class FrasePreferida 
+{
+    public static void main(String[] args)
+    {
+        System.out.println("A esperanca e uma cancao silenciosa, que seu coracao pode cantar apesar de tudo.");
+    }
+}
